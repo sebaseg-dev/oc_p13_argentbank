5ce6f4125f6cff6e54176d589f91229f056dd542
sedomu <seguin@lilo.org>
2025-08-06 10:26:34 +0200

    refactoring <Header/>: logic for LoginButtons.jsx separated into a specific component file for more clarity

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,16 +1,7 @@
 import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { toggleConnected } from '../redux.js'
-import { useNavigate } from 'react-router-dom'
+import LoginButtons from './LoginButtons.jsx'
 
 export default function Header() {
-  const userLogin = useSelector(state => state.userLogin)
-  console.log(userLogin)
-
-  const navigate = useNavigate()
-
-  const dispatch = useDispatch()
-
   return <>
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
@@ -21,26 +12,7 @@ export default function Header() {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
-        {userLogin.connected ?
-          <div className="main-nav-menu">
-            <Link className="main-nav-item" to="/profile">
-              <i className="fa fa-user-circle"></i>
-              {userLogin.user.firstName}
-            </Link>
-            <Link to="#" className="main-nav-item" onClick={() => {
-              dispatch(toggleConnected())
-              navigate('/')
-            }}>
-              <i className="fa fa-sign-out"></i>Sign Out
-            </Link>
-          </div> :
-          <div className="main-nav-menu">
-            <Link className="main-nav-item" to="/login">
-              <i className="fa fa-user-circle"></i>
-              Sign In
-            </Link>
-          </div>
-        }
+      <LoginButtons/>
     </nav>
   </>
 }
