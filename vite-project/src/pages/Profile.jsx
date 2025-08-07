72b2fa8bd2a2609086af150250de676d5fb2bf0c
sedomu <seguin@lilo.org>
2025-08-07 08:42:20 +0200

    creating a placeholder component containing placeholder data for more readability

--- vite-project/src/pages/Profile.jsx
+++ vite-project/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 import apiGetUserInformation from '../services/apiGetUserInformation.js'
 import apiPutUserInformation from '../services/apiPutUserInformation.js'
+import { PlaceholderProfileAccounts } from '../components/PlaceholderData.jsx'
 
 export default function Profile() {
   document.title = 'Argent Bank - Profile Page'
@@ -72,37 +73,9 @@ export default function Profile() {
         )}
 
       </div>
-      <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+
+      <PlaceholderProfileAccounts/>
+
     </main>
 
     <Footer/>
